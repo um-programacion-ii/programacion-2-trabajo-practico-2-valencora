74ce72428a21d2db24bc27048bd683bf6471ad24
valencora <coratolovalen@gmail.com>
2025-04-16 10:55:40 -0300

    busqueda por titulo

--- Bibliotecadigital/src/GestorRecursos.java
+++ Bibliotecadigital/src/GestorRecursos.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GestorRecursos {
     private List<RecursoDigital> recursos;
@@ -8,6 +9,12 @@ public class GestorRecursos {
     public GestorRecursos(ServicioNotificaciones servicioNotificaciones) {
         this.recursos = new ArrayList<>();
         this.servicioNotificaciones = servicioNotificaciones;
+    }
+
+    public List<RecursoDigital> buscarPorTitulo(String titulo) {
+        return recursos.stream()
+                .filter(r -> r.getTitulo().toLowerCase().contains(titulo.toLowerCase()))
+                .collect(Collectors.toList());
     }
 
     public void agregarRecurso(RecursoDigital recurso) {
